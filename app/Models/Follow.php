aa526cd947cf219889fcbf6efc38bc4b75eb4ac8
Raphael <rjjaggerd@gmail.com>
2023-11-13 20:25:15 +0300

    Added userDoingTheFollowing and userBeingFollowed Methods to the follow Model

--- app/Models/Follow.php
+++ app/Models/Follow.php
@@ -5,7 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Follow extends Model
-{
-    use HasFactory;
+class Follow extends Model {
+  use HasFactory;
+
+  // Get users following the current user and set them to belong to the current user.
+  // user_id in the follow method is the user doing the following
+  public function userDoingTheFollowing() {
+    return $this->belongsTo(User::class, 'user_id');
+  }
+
+  // We get the user being followed by current user and set them belongin to that user.
+  // followeduser in the Follow model is the user being followed.
+  public function  userBeingFollowed() {
+    return $this->belongsTo(User::class, 'followeduser');
+  }
 }
