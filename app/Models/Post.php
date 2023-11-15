b408e02b5f24c6da08d39014ec629447991eeb8a
Raphael <rjjaggerd@gmail.com>
2023-11-15 15:56:18 +0300

    Enabled Post model to be Searchable using Laravel Scout package

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Post extends Model {
+
+  use Searchable;
   use HasFactory;
 
   protected $fillable = ['title', 'body', 'user_id'];
@@ -14,5 +17,13 @@ class Post extends Model {
     // Return a relationship between post and user
     // We are saying a blog post belongs to a user.
     return $this->belongsTo(User::class, 'user_id');
+  }
+
+  // spells out what shoud be searchable on the databse row
+  public function toSearchableArray() {
+    return [
+      'title' => $this->title,
+      'body' => $this->body
+    ];
   }
 }
