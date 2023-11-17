97f285c18e9614768af9ba6159013c303803eb95
Raphael <rjjaggerd@gmail.com>
2023-11-17 19:58:16 +0300

    Added chat feature but did not include pusher credentials or create pusher account

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,12 @@
 import './bootstrap';
 import Search from './live-search';
+import Chat from './chat';
 
+// A new chat instance is only created if the search icon is present.
 if (document.querySelector('.header-search-icon')) {
   new Search();
+}
+
+if (document.querySelector('.header-chat-icon')) {
+  new Chat();
 }
